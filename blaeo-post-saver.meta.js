4d647f3e6dbaa7485dc295192bccb8ae969804e8
kubikill <kuboloui@gmail.com>
2020-05-01 12:07:43 +0200

    Fix URLs

--- blaeo-post-saver.meta.js
+++ blaeo-post-saver.meta.js
@@ -4,8 +4,8 @@
 // @version      1.0.0
 // @homepage     https://github.com/kubikill/blaeo-post-saver#readme
 // @supportURL   https://github.com/kubikill/blaeo-post-saver/issues
-// @updateURL    https://kubikill.github.io/blaeo-post-saver/blaeo-post-saver.meta.js
-// @downloadURL  https://kubikill.github.io/blaeo-post-saver/blaeo-post-saver.user.js
+// @updateURL    https://github.com/kubikill/blaeo-post-saver/raw/master/blaeo-post-saver.meta.js
+// @downloadURL  https://github.com/kubikill/blaeo-post-saver/raw/master/blaeo-post-saver.user.js
 // @description  Adds a save/load feature to BLAEO posts.
 // @author       kubikill
 // @match        https://www.backlog-assassins.net/posts/new
